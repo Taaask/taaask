42d095f4ec8fe0a68b676d40a2792b44259943d3
yashsehgal <yashsehgal.work@gmail.com>
2021-11-04 20:41:16 +0530

    feat(wip): discard changes from edit-mode feature added

--- src/components/modals/ChecklistTasks.js
+++ src/components/modals/ChecklistTasks.js
@@ -1,12 +1,21 @@
 import { useState } from "react"
+import ReactModal from 'react-modal';
 
 // styling objects
 const InputStyle = {
   width: '100%'
 }
 
+const CTAButtonsWrapper = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: '0.8em'
+};
+
 export default function ChecklistTasks(__ChecklistDetails) {
   const [editChecklistDetailsModeState, setEditChecklistDetailsModeState] = useState('no-edit-mode');
+  const [discardConfirmationModalState, setDiscardConfirmationModalState] = useState(false);
   if (editChecklistDetailsModeState === 'no-edit-mode') {
     return (
       <div className="checklist-tasks-details-container">
@@ -61,6 +70,11 @@ export default function ChecklistTasks(__ChecklistDetails) {
                 setEditChecklistDetailsModeState('no-edit-mode');
               }}
             >Save changes</button>
+            <button className="outline-btn border-color-purple-900 color-purple-900"
+              onClick={() => {
+                setDiscardConfirmationModalState(true);
+              }}
+            >Discard Changes</button>
           </div>
         </div>
         <div className="checklist-details-edit-input-wrapper">
@@ -89,6 +103,39 @@ export default function ChecklistTasks(__ChecklistDetails) {
               defaultValue={__ChecklistDetails.checklist_labels_data} />
           </div>
         </div>
+        <ReactModal
+          isOpen={discardConfirmationModalState}
+          onRequestClose={() => setDiscardConfirmationModalState(false)}
+          style={{
+            overlay: {
+              backgroundColor: 'var(--neutral-gray-500)'
+            },
+            content: {
+              width: 'fit-content',
+              height: 'fit-content',
+              padding: '1.6em',
+              margin: 'auto',
+              boxShadow: '0px 6px 20px #00000045'
+            }
+          }}
+        >
+          <h2>Are you sure you want to discard/exit?</h2>
+          <div className="cta-buttons-wrapper m-top-3"
+            style={CTAButtonsWrapper}
+          >
+            <button className="primary-btn bg-color-purple-900"
+              onClick={() => setDiscardConfirmationModalState(false)}
+            >No, don't discard the changes</button>
+            <button className="outline-btn border-color-purple-900 color-purple-900"
+              onClick={() => {
+                setDiscardConfirmationModalState(false);
+                setEditChecklistDetailsModeState('no-edit-mode');
+              }}
+            >
+              Yes, discard these changes
+            </button>
+          </div>
+        </ReactModal>
       </div>
     )
   }
