b94596875683ed6fdb3210c9cd646719ba225736
yashsehgal <yashsehgal.work@gmail.com>
2021-11-13 01:46:55 +0530

    feat: routing logics added for user-details

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Checklists from './pages/Checklists';
 import Home from './pages/Home';
+import { getUserDetails, saveUserDetails } from './utils/LocalStorage';
+import UserDetails from './pages/UserDetails';
 import './styles/index.css';
+import Footer from './components/Footer';
 
 export default function App() {
+  checkLocalStorageUserDetails();
   return (
     <div className="app">
       <BrowserRouter>
@@ -12,8 +16,25 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
 	  		  <Route path="/checklists" element={<Checklists />} />
+          <Route path="/user-details" element={<UserDetails />} />
 	  	  </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
+}
+
+// method to route data according to the data in local-storage
+function checkLocalStorageUserDetails() {
+  console.log(getUserDetails());
+  if (getUserDetails() === undefined) {
+    window.location.href = "/user-details";
+  } else if (getUserDetails() === null) {
+    if (saveUserDetails({})) {
+      console.log("user-details data has been added to local-storage array");
+    } else {
+      alert('user details handler is not working properly, close the web app and try again to re-run');
+    }
+    window.location.href = "/user-details";
+  }
 }
