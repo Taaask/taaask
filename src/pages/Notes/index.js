88a5a0ba2aa40ff34e706a05a70a043040f21513
yashsehgal <yashsehgal.work@gmail.com>
2021-11-14 00:33:35 +0530

    feat: recently-added-notes feature layout added (wip)

--- src/pages/Notes/index.js
+++ src/pages/Notes/index.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import RecentlyCreatedNotes from './RecentlyCreatedNotes';
 
 export default function Notes() {
   return (
@@ -8,6 +9,16 @@ export default function Notes() {
           <i className="fas fa-arrow-left"></i>
         </button>
       </Link>
+      <h3 className="neutral-gray-900 m-top-2">Home / Notes</h3>
+      {/* recent notes section */}
+      <div className="recent-notes-section m-top-5">
+        <p className="recent-notes-section-title neutral-gray-500">
+          All of your created Notes
+        </p>
+        <div className="recent-notes-wrapper">
+          <RecentlyCreatedNotes />
+        </div>
+      </div>
     </div>
   )
 }
